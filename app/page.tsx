6012fbe13d3e5a4ed79939131a0733f93615fb71
Mirzaadr <mirzaadr34@yahoo.com>
2025-01-27 12:08:29 +0700

    generate title based on filter in home

--- app/page.tsx
+++ app/page.tsx
@@ -17,6 +17,19 @@ interface HomePageProps {
   }>;
 }
 
+function getTitle({ q, type, location, remote }: JobFilterValues) {
+  const titlePrefix = q
+    ? `${q} jobs`
+    : type
+      ? `${type} developer jobs`
+      : remote
+        ? "Remote developer jobs"
+        : "All developer jobs";
+  const titleSuffix = location ? ` in ${location}` : "";
+
+  return `${titlePrefix}${titleSuffix}`;
+}
+
 export default async function Home({ searchParams }: HomePageProps) {
   const params = await searchParams;
   const filterValues: JobFilterValues = {
@@ -28,7 +41,7 @@ export default async function Home({ searchParams }: HomePageProps) {
       {/* <Navbar /> */}
       <main className="m-auto my-10 max-w-5xl space-y-10 px-3">
         <div className="space-y-5 text-center">
-          <H1>Developer Jobs</H1>
+          <H1>{getTitle(filterValues)}</H1>
           <p className="text-muted-foreground">Find your dream job</p>
         </div>
         <section className="flex flex-col gap-4 md:flex-row">

648d93d4b4475905d1d7b4b6093cc3554dd30223
Mirzaadr <mirzaadr34@yahoo.com>
2025-01-28 19:16:41 +0700

    handle user role

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import authConfig from "./auth.config";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { db } from "@/lib/prisma";
+import { getUserById } from "./data/user";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(db),
@@ -15,12 +16,16 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
   callbacks: {
     ...authConfig.callbacks,
     async jwt({ token, user }) {
-      if (user) {
-        token.id = user.id;
-        token.name = user.name;
-        token.email = user.email;
-      }
+      if (!token.sub) return token;
 
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+
+      token.name = existingUser.name;
+      token.email = existingUser.email;
+      token.role = existingUser.role;
+      token.isOAuth = !!(existingUser?.accounts.length > 0);
       return token;
     },
   },

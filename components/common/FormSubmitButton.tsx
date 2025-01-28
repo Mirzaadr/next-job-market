d9373014a857dc7819465e98fad3645994afbb67
Mirzaadr <mirzaadr34@yahoo.com>
2025-01-28 19:15:30 +0700

    edit form submit button

--- components/common/FormSubmitButton.tsx
+++ components/common/FormSubmitButton.tsx
@@ -2,13 +2,21 @@
 
 import { useFormStatus } from "react-dom";
 import LoadingButton from "./LoadingButton";
+import { cn } from "@/lib/utils";
 
 const FormSubmitButton = ({
   className,
   ...props
 }: React.ButtonHTMLAttributes<HTMLButtonElement>) => {
   const { pending } = useFormStatus();
-  return <LoadingButton {...props} type="submit" loading={pending} />;
+  return (
+    <LoadingButton
+      {...props}
+      className={cn("", className)}
+      type="submit"
+      loading={pending}
+    />
+  );
 };
 
 export default FormSubmitButton;

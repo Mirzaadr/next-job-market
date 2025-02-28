a6fdf6233d9abe1d1536c204cbda0826a1fd1fcf
Mirzaadr <mirzaadr34@yahoo.com>
2025-02-28 16:40:29 +0700

    add suspense to signin

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -1,9 +1,19 @@
 import SigninForm from "@/components/auth/SigninForm";
+import Spinner from "@/components/common/Spinner";
+import { Suspense } from "react";
 
 const SigninPage = () => {
   return (
-    <SigninForm />
-  )
-}
+    <Suspense
+      fallback={
+        <div>
+          <Spinner />
+        </div>
+      }
+    >
+      <SigninForm />
+    </Suspense>
+  );
+};
 
 export default SigninPage;
--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,9 +1,19 @@
 import SignupForm from "@/components/auth/SignupForm";
+import Spinner from "@/components/common/Spinner";
+import { Suspense } from "react";
 
 const SignupPage = () => {
   return (
-    <SignupForm />
-  )
-}
+    <Suspense
+      fallback={
+        <div>
+          <Spinner />
+        </div>
+      }
+    >
+      <SignupForm />
+    </Suspense>
+  );
+};
 
 export default SignupPage;
